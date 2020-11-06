aab562e96f95db256f87807cdea08cbb9679d941
antossio <antossio@hotmail.com>
2020-11-07 01:27:05 +0300

    Исправление расположение вывода

--- Lesson_06/Homework_Theme_01/Program.cs
+++ Lesson_06/Homework_Theme_01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,25 +45,22 @@ namespace Homework_Theme_01
             double rP;           // Оценка по русскому языку
             double averagePoint; // Средний балл по всем предметам
 
-            string pattern = "Ваше имя: {0}, Ваш возраст: {1}, Ваш рост: {2}, Ваша оценка по истории: {3}, Ваша оценка по математике: {4}, Ваша оценка по русскому языку: {5}, Ваш средний балл: {6:0.0}"; // создаем шаблон для форматированного вывода
-
-
-            Console.WriteLine("Введите ваше имя:");
+            Console.WriteLine("Введите имя:");
             fullname = Console.ReadLine(); // записываем имя
 
-            Console.WriteLine("Введите ваш возраст:");
+            Console.WriteLine("Введите возраст:");
             age = ushort.Parse(Console.ReadLine()); // преобразуем в ushort и записываем возраст
 
-            Console.WriteLine("Введите ваш рост:");
+            Console.WriteLine("Введите рост:");
             height = uint.Parse(Console.ReadLine()); // преобразуем в uint и записываем рост
 
-            Console.WriteLine("Введите вашу оценку по истории:");
+            Console.WriteLine("Введите балл по истории:");
             hP = double.Parse(Console.ReadLine()); // преобразуем в double и записываем оценку по истории
 
-            Console.WriteLine("Введите вашу оценку по математике:");
+            Console.WriteLine("Введите балл по математике:");
             mP = double.Parse(Console.ReadLine()); // преобразуем в double и записываем оценку по математике
 
-            Console.WriteLine("Введите вашу оценку по русскому языку:");
+            Console.WriteLine("Введите балл по русскому языку:");
             rP = double.Parse(Console.ReadLine()); // преобразуем в double и записываем оценку по русскому языку
 
 
@@ -71,22 +69,26 @@ namespace Homework_Theme_01
             /// </summary>
             averagePoint = (hP + mP + rP) / 3;
 
-            string formatPattern = pattern;
+            string output = "---Обычный вывод---"; 
+            string pattern = fullname + age + height + hP + mP + rP + averagePoint; // создаем шаблон для обычного вывода
 
-            string interpolPattern = ($"Ваше имя: {fullname}, Ваш возраст: {age}, Ваш рост: {height}, Ваша оценка по истории: {hP}, Ваша оценка по математике: {mP}, Ваша оценка по русскому языку: {rP}, Ваш средний балл: {averagePoint:0.0}");
+            string formatOutput = "---Форматированный вывод---";
+            string formatPattern = String.Format("Имя: {0}, Возраст: {1}, Рост: {2}, Балл по истории: {3}, Балл по математике: {4}, Балл по русскому языку: {5}, Средний балл: {6:0.0}", fullname, age, height, hP, mP, rP, averagePoint); // создаем шаблон для форматированного вывода
 
+            string interpolOutput = "---Интерполяция строк---";
+            string interpolPattern = ($"Имя: {fullname}, Возраст: {age}, Рост: {height}, Балл по истории: {hP}, Балл по математике: {mP}, Балл по русскому языку: {rP}, Средний балл: {averagePoint:0.0}"); // создаем шаблон для вывода с интерполяцией
 
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2 - 3); // расположение курсора по центру экрана
-            Console.WriteLine("---Обычный вывод---"); // выводим в консоль обычный вывод
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2 - 2); // расположение курсора по центру экрана
-            Console.WriteLine(fullname + age + height + hP + mP + rP + averagePoint);
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2); // расположение курсора по центру экрана
-            Console.WriteLine("---Форматированный вывод---"); // выводим в консоль форматированный вывод  
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2 + 1); // расположение курсора по центру экрана
-            Console.WriteLine (formatPattern);
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2 + 4); // расположение курсора по центру экрана
-            Console.WriteLine("---Интерполяция строк---"); // выводим в консоль вывод с интерполяцией строк
-            Console.SetCursorPosition((Console.WindowWidth / 2), Console.WindowHeight / 2 + 5); // расположение курсора по центру экрана
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (output.Length / 2), Console.WindowHeight / 2 - 3); // расположение курсора по центру экрана
+            Console.WriteLine(output); // выводим в консоль обычный вывод
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (pattern.Length / 2), Console.WindowHeight / 2 - 2); // расположение курсора по центру экрана
+            Console.WriteLine(pattern);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (formatOutput.Length / 2), Console.WindowHeight / 2); // расположение курсора по центру экрана
+            Console.WriteLine(formatOutput); // выводим в консоль форматированный вывод  
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (formatPattern.Length / 2), Console.WindowHeight / 2 + 1); // расположение курсора по центру экрана
+            Console.WriteLine(formatPattern);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (interpolOutput.Length / 2), Console.WindowHeight / 2 + 4); // расположение курсора по центру экрана
+            Console.WriteLine(interpolOutput); // выводим в консоль вывод с интерполяцией строк
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (interpolPattern.Length / 2), Console.WindowHeight / 2 + 5); // расположение курсора по центру экрана
             Console.WriteLine(interpolPattern);
             Console.ReadKey();
 
